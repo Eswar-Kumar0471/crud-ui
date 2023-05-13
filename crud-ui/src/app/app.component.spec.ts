d7249cefb8414e1479807225bd9e530d9254e672
Eswar Kumar <83254184+Eswar-Kumar0471@users.noreply.github.com>
2023-05-13 10:49:30 +0530

    Project created
    Project created

--- crud-ui/src/app/app.component.spec.ts
+++ crud-ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crud-employee-management'`, () => {
+  it(`should have as title 'crud-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud-employee-management');
+    expect(app.title).toEqual('crud-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crud-employee-management app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crud-ui app is running!');
   });
 });
